3821315ff559a8100b3ce8131a2c748bcfddc7ab
SrishamSampekatte <srishamsm.xworkz@gmail.com>
2024-10-11 22:42:52 +0530

    List of users who have access to Repository

---
+++ github-api/list_users.sh
@@ -0,0 +1,61 @@
+
+#!/bin/bash
+
+
+
+#Author : Srisham S M 
+#Created date : 11/10/2024
+
+
+#Github api url 
+
+API_URL="https://api.github.com"
+
+
+#Github user name and token
+
+USERNAME=$username
+TOKEN=$token
+
+# User and Repository information
+REPO_OWNER=$1
+REPO_NAME=$2
+
+
+#Function to make a GET Request from Github API
+
+function github_api_get {
+
+local endpoint=$1
+local url="${API_URL}/${endpoint}"
+
+#send a GET Request to Github API with  Authentication
+
+curl -s -u "${USERNAME}:${TOKEN}" "url"
+
+}
+
+function list_users_with_read_access { 
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+           # Fetch the list of collaborators on the repository
+	       collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+
+	           # Display the list of collaborators with read access
+		       if [[ -z "$collaborators" ]]; then
+		               echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+			           else
+				           echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+					           echo "$collaborators"
+						       fi
+
+
+}
+
+# Main script
+
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+list_users_with_read_access
+
+
+
